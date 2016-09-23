7711973880425163d4a1724d6e54e5bd7ad6c301
Matt Peleti <mattpeleti@gmail.com>
2016-09-23 23:26:20 +0000

    roll of the die

--- dice_roll.rb
+++ dice_roll.rb
@@ -1,7 +1,9 @@
 # Create method `roll` that returns a random number between 1 and 6
-
 # Feel free to google "how to generate a random number in ruby"
 
+
+
 def roll
-  # code goes here
+  dice=Random.new
+  dice.rand(1..6)
 end
